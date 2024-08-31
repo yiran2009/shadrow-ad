924fb956840afabfa2a625feb1914332d906126c
yiran2009 <38534834+yiran2009@users.noreply.github.com>
2024-08-31 21:22:49 +0800

    Update huwaicd.js

--- huwaicd.js
+++ huwaicd.js
@@ -1,1 +1,8 @@
+let body = $response.body;
+
+// 使用正则表达式替换 "amount" 和 "laterAmount" 的值
+body = body.replace(/"amount"\s*:\s*\d+, '"amount": 6500');
+body = body.replace(/"laterAmount"\s*:\s*\d+, '"laterAmount": 7000');
+
+$done({ body });
 
